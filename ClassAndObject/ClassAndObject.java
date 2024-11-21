c223b1a1874cbecbf97c6e9b93cd14aedbe9983f
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2024-11-21 23:24:14 +0600

    Add files via upload

---
+++ ClassAndObject/ClassAndObject.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.studyopedia.classandobject;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class ClassAndObject {
+    String name, gender;
+    int age;
+    void display()
+    {
+        System.out.println("New method");
+    }
+    
+
+    
+}
---
+++ ClassAndObject/ClassAndObject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studyopedia.classandobject;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class ClassAndObject1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ClassAndObject e= new ClassAndObject();
+        e.name="mahfujul";
+        e.gender="male";
+        e.age=25;
+        e.display();
+        System.out.println("\nname:"+e.name+"\nage:"+e.age+"\nGender:"+e.gender);
+    }
+    
+}
