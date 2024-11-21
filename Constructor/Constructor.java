3df003b8bde77e11733197cfbc205c485baa3995
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2024-11-21 23:26:04 +0600

    Add files via upload

---
+++ Constructor/Constructor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.studyopedia.constructor;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class Constructor {
+
+   String name, gender;
+   int age;
+   Constructor(String n, String s, int a){
+       name=n;
+       gender=s;
+       age=a;
+   }
+}
---
+++ Constructor/Parameterized.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studyopedia.constructor;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class Parameterized {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Constructor e =new Constructor("Toushif","Male", 25);
+        System.out.println("\nname:"+e.name+"\nage:"+e.age+"\nsex:"+e.gender);
+    }
+             
+}
