de709a00a7245ec92a382d914b726bc3517fb2f3
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2024-11-21 23:27:10 +0600

    Add files via upload

---
+++ DefaultConstructor/Constructor1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.studyopedia.constructor1;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class Constructor1 {
+
+    String name, gender;
+    int age;
+    Constructor1(){
+        System.out.println("Default Constructor");
+    }
+}
---
+++ DefaultConstructor/Default.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studyopedia.constructor1;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class Default {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Constructor1 e = new Constructor1();
+        e.name="mahfujul";
+        e.gender="Male";
+        e.age=23;
+        System.out.println("\nname:"+e.name+"\nage:"+e.age+"\ngender:"+e.gender);
+    }
+    
+}
