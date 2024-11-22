113d29d1cb7c009f032ebaef711039b4281f1075
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2024-11-22 21:58:22 +0600

    Add files via upload

---
+++ Interface1/A6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studyopedia.interface1;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class A6 implements Interface1 {
+    public void print(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        A6 obj= new A6();
+        obj.print();
+    }
+    
+}
---
+++ Interface1/Interface1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.studyopedia.interface1;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public interface Interface1 {
+
+    void print();
+}
