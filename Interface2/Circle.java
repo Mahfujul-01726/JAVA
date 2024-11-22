fbc42079620ffd024b3f7b58c79dbc89f27f6654
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2024-11-22 21:59:31 +0600

    Add files via upload

---
+++ Interface2/Circle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studyopedia.interface2;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class Circle implements Interface2 {
+    @Override
+    public void draw(){
+        System.out.println("drawing circle");
+    }
+}
---
+++ Interface2/TestInterface1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.studyopedia.interface2;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class TestInterface1 {
+    public static void main(String args[]){
+        Interface2 d=new Circle();
+        d.draw();
+    }
+    
+}
