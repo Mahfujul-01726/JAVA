3eaaaf0545918d73cc71d307c406366a8e2fccd1
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2024-11-22 22:09:45 +0600

    Add files via upload

---
+++ ForEachExample/ForEachExample.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.studyopedia.foreachexample;
+
+/**
+ *
+ * @author MD MAHFUJUL KARIM
+ */
+public class ForEachExample {
+
+    public static void main(String[] args) {
+//        Declaring an array
+          int arr[]={12,23,44,56,78};
+          // Printing array using for each loop
+          for(int i:arr){
+              System.out.println(i);
+          }
+        
+    }
+}
